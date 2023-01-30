6c5a5765b6be233ee539a8a469a577dbdc831127
chughtai77 <60922177+chughtai77@users.noreply.github.com>
2023-01-31 01:59:46 +0500

    Add project files.

---
+++ RestaurantWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using RestaurantWeb.Models;
+
+namespace RestaurantWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+	
+		}
+		public DbSet<Category> Categories { get; set; }
+	}
+}
